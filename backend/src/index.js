1ffcfa1da065dc46cd771ccea662e9dede1fde5b
sonikakannan <sonikakannan66@gmail.com>
2025-05-27 12:11:47 +0530

    first commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    ""
+    "https://sign-language-visual-dictionary-1.onrender.com"
   ],
   credentials: true,
 }));
